7ecd3568578ae0337b7c52e87f3be5545db381ea
なりかくん <narikakun@gmail.com>
2021-09-12 17:42:00 +0900

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace MatchanDiscord
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(168, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(456, 64);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "まっちゃんウイルス";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(238, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(318, 35);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "まちゃウイ制作委員会";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 123);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "まっちゃんウイルス [Discord版]";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using DiscordRPC;
+using DiscordRPC.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Button = DiscordRPC.Button;
+
+namespace MatchanDiscord
+{
+    public partial class Form1 : Form
+    {
+		public DiscordRpcClient client;
+
+		private static RichPresence presence = new RichPresence()
+		{
+			Details = "まっちゃんに感染なう",
+			State = "まちゃウイ製作委員会",
+			Assets = new Assets()
+			{
+				LargeImageKey = "67ea35031cae9961",
+				LargeImageText = "まっちゃんだよ！",
+				SmallImageKey = "e002d5a2dd18e24db672b017a6865cb3"
+			},
+			Party = new Party()
+			{
+				ID = Secrets.CreateFriendlySecret(new Random()),
+				Max = 114514,
+				Size = 1919,
+				Privacy = Party.PrivacySetting.Public
+			},
+			Timestamps = Timestamps.FromTimeSpan(114514)
+		};
+
+		public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new DiscordRpcClient("886522791912947722");
+			client.Logger = new ConsoleLogger() { Level = LogLevel.Warning };
+			presence.Secrets = new Secrets()
+			{
+				JoinSecret = "join_matchaan",
+				SpectateSecret = "gahaha"
+			}; ;
+			client.RegisterUriScheme();
+			client.SetSubscription(EventType.Join | EventType.Spectate | EventType.JoinRequest);
+			client.SetPresence(presence);
+			client.Initialize();
+		}
+	}
+}
